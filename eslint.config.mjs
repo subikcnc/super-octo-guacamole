ef839a8f9b90efcbe945c80ea0a4a78ba20c8cfc
subikcnc <110371013+subikcnc@users.noreply.github.com>
2025-06-15 11:53:23 +0545

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginImport from 'eslint-plugin-import';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    files: ['**/*.{js,cjs,mjs,ts,tsx}'],
+    ignores: ['components/ui/'],
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      camelCase: 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'index',
+            'object',
+          ],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              pattern: '@app/',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'no-undef': 'off', // Needed if using TypeScript
+    },
+  },
+];
+
+export default eslintConfig;

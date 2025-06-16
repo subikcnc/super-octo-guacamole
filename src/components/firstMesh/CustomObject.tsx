789080bb4a6f6cdc5bbaab5be1f3d98f80cc87d9
cnc <subik@curvesncolors.ch>
2025-06-16 14:31:21 +0545

    added usememo

--- src/components/firstMesh/CustomObject.tsx
+++ src/components/firstMesh/CustomObject.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import { useMemo } from 'react';
+import * as THREE from 'three';
 
 const CustomObject = () => {
   const verticesCount = 10 * 3; // 10 triangles with 3 vertices per triangle
-  const positions = new Float32Array(verticesCount * 3); // 3 values per vertex
+  const positions = useMemo(() => {
+    const positions = new Float32Array(verticesCount * 3);
+    for (let i = 0; i < verticesCount * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 3;
+    }
+    return positions;
+  }, [verticesCount]); // 3 values per vertex
 
-  for (let i = 0; i < verticesCount * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 3;
-  }
   return (
     <mesh>
       <bufferGeometry>
         <bufferAttribute attach="attributes-position" args={[positions, 3]} />
       </bufferGeometry>
-      <meshBasicMaterial color="red" />
+      <meshBasicMaterial color="red" side={THREE.DoubleSide} />
     </mesh>
   );
 };
